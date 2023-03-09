de219554280e4cf3900a780a7689c1aa6dd6ddba
JuankDev <11jcat11@gmail.com>
2023-03-08 22:00:39 -0400

    Agregar archivos de proyecto.

---
+++ BAComentarios/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BAComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BAComentarios
+{
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set;}
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
---
+++ BAComentarios/Controllers/ComentarioController.cs
@@ -0,0 +1,119 @@
+﻿using BAComentarios.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BAComentarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public ComentarioController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listComentarios = await _context.Comentario.ToListAsync();
+                return Ok(listComentarios);
+            }
+            catch (System.Exception exc)
+            {
+                return BadRequest(exc.Message);
+            }
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var comentario = await _context.Comentario.FindAsync(id);
+                if (comentario == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(comentario);
+                }
+            }
+            catch (System.Exception exc)
+            {
+                return BadRequest(exc.Message);
+
+            }
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Comentario comentario)
+        {
+            try
+            {
+                _context.Add(comentario);
+                await _context.SaveChangesAsync();
+                return Ok(comentario);
+            }
+            catch (System.Exception exc)
+            {
+                return BadRequest(exc.Message);
+
+            }
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Comentario comentario)
+        {
+            try
+            {
+                if (id != comentario.Id)
+                {
+                    return BadRequest();
+                }
+                
+                _context.Update(comentario);
+                await _context.SaveChangesAsync();
+                return Ok( new { message = "Comentario actualizado con exito"});
+            }
+            catch (System.Exception exc)
+            {
+                return BadRequest(exc.Message);
+
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var comentario = await _context.Comentario.FindAsync(id);
+                if (comentario == null)
+                {
+                    return NotFound();
+                }
+                _context.Comentario.Remove(comentario);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Comentario eliminado con exito!!" });
+            }
+            catch (System.Exception exc)
+            {
+                return BadRequest(exc.Message);
+
+            }
+        }
+    }
+}
